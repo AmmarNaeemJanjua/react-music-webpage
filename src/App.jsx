3c85c89abaf9e7805d1ebbb00ead958c2915861e
AmmarNaeemJanjua <anjanjua99@gmail.com>
2024-10-16 20:18:26 +0500

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "@/views/home-page/index.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <HomePage />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <HomePage />
+      <RouterProvider router={router} />
     </>
   );
 }
